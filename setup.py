e26bee90883b483a2d6d3a3defff3fac19a17c96
eitam ring <eitam.ring@kubemq.io>
2019-10-07 14:22:24 +0100

    Addeded _init file

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setup
+from setuptools import setuptools
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -9,8 +9,8 @@ README = (HERE / "README.md").read_text()
 
 
 
-setup(name='kubemq',
-      version='1.0.2',
+setuptools.setup(name='kubemq',
+      version='1.0.4',
       description='KubeMQ SDK for Python',
 	  long_description=README,
 	  long_description_content_type="text/markdown",
@@ -18,16 +18,7 @@ setup(name='kubemq',
       author='KubeMQ',
       author_email='info@kubemq.io',
       license='MIT',
-      packages=[
-          'kubemq',
-          'kubemq.basic',
-          'kubemq.commandquery',
-          'kubemq.commandquery.lowlevel',
-          'kubemq.events',
-          'kubemq.events.lowlevel',
-          'kubemq.grpc',
-          'kubemq.subscription',
-      ],
+      packages=setuptools.find_packages(),
       install_requires=[
           'future==0.17.1',
           'grpcio==1.17.1',
