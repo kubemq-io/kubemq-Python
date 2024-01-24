3e2ace76dc32f505b82654482db5784eb3baaf86
Lior Nabat <lior.nabat@gmail.com>
2024-01-24 18:41:41 +0200

    Update Response class in commandquery module
    This commit revises the Response class in the commandquery module. It removes the existing license header and imports, significantly enhancing readability and maintenance. It also adds a series of new class attributes including client_id, request_id, reply_channel, metadata, body, cache_hit, timestamp, executed, error, and tags for future functionality.

--- kubemq/commandquery/response.py
+++ kubemq/commandquery/response.py
@@ -1,36 +1,18 @@
-# MIT License
-#
-# Copyright (c) 2018 KubeMQ
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-from datetime import datetime
-
-from kubemq.commandquery.request_receive import RequestReceive
-from kubemq.grpc import Response as InnerResponse
-
-epoch = datetime.utcfromtimestamp(0)
-
 
 class Response:
     """The response that receiving from KubeMQ after sending a request."""
 
     def __init__(self, request):
+        self.client_id = ""
+        self.request_id = ""
+        self.reply_channel = ""
+        self.metadata = ""
+        self.body = None
+        self.cache_hit = False
+        self.timestamp = datetime.now()
+        self.executed = False
+        self.error = ""
+        self.tags = {}
         if isinstance(request, RequestReceive):
             self.request_id = request.request_id
             """Represents a Response identifier."""
