47e2cffc41448f672e83415612afefc9ff50a60c
Lior Nabat <lior.nabat@gmail.com>
2024-01-24 18:41:54 +0200

    Correct import statements and function call in setup.py
    The import statement from setuptools has been corrected and the function call to find_packages has been updated. The version of the 'kubemq' package has also been updated from '2.3.0' to '2.3.1'. These changes improve the overall code quality and maintainability.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setuptools
+from setuptools import setup, find_packages  # Corrected import statement
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -7,22 +7,22 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-
-
-setuptools.setup(name='kubemq',
-      version='2.3.0',
-      description='KubeMQ SDK for Python',
-	  long_description=README,
-	  long_description_content_type="text/markdown",
-      url='https://github.com/kubemq-io/kubemq-Python',
-      author='KubeMQ',
-      author_email='info@kubemq.io',
-      license='MIT',
-      packages=setuptools.find_packages(),
-      install_requires=[
-          'grpcio>=1.51.1',
-          'protobuf>=4.21.0',
-          'setuptools>=40.8.0',
-          'PyJWT>=2.6.0',
-      ],
-      zip_safe=False)
+setup(
+    name='kubemq',
+    version='2.3.1',
+    description='KubeMQ SDK for Python',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/kubemq-io/kubemq-Python',
+    author='KubeMQ',
+    author_email='info@kubemq.io',
+    license='MIT',
+    packages=find_packages(),  # Corrected function call
+    install_requires=[
+        'grpcio>=1.51.1',
+        'protobuf>=4.21.0',
+        'setuptools>=40.8.0',
+        'PyJWT>=2.6.0',
+    ],
+    zip_safe=False
+)
