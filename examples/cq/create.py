580bf621f66e2f185e60b9b617838c25f3495ac4
Lior Nabat <lior.nabat@gmail.com>
2024-03-11 13:49:19 +0200

    Add new commands and queries handling script examples
    Introduced new script examples to demonstrate usage of commands and queries in the 'kubemq' module. These scripts emphasize on creating, listing, and deleting channels, and sending query or command requests. Exception handling is also included for each operation to ensure robustness and reliability of the system.

---
+++ examples/cq/create.py
@@ -0,0 +1,24 @@
+import os
+from kubemq.cq import *
+
+
+def create_commands_channel(channelName):
+    try:
+        with Client(address="localhost:50000") as client:
+            client.create_commands_channel(channelName)
+            print(f"Commands channel: {channelName} created successfully.")
+    except Exception as e:
+        print(f"Error creating Commands Channel: {channelName},  {e}")
+
+
+def create_queries_channel(channelName):
+    try:
+        with Client(address="localhost:50000") as client:
+            client.create_queries_channel(channelName)
+            print(f"Queries channel: {channelName} created successfully.")
+    except Exception as e:
+        print(f"Error creating Queries Channel: {channelName},  {e}")
+
+if __name__ == "__main__":
+    create_commands_channel("new_commands_channel")
+    create_queries_channel("new_queries_channel")
