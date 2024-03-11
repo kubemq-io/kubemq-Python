33ecfa7bdf0a63e46e4a20d98c9d75f58c6013a2
Lior Nabat <lior.nabat@gmail.com>
2024-03-11 15:29:44 +0200

    Add create, delete, list scripts and refactor queue example
    Three scripts for creating, deleting, and listing queue channels have been added to aid in queue management. Additionally, the 'queues.py' example has been refactored to use the new 'kubemq.queues' module, improving code clarity and efficiency. Lastly, the example files have been reorganized into a new 'queues' directory for better file management.

---
+++ examples/queues/create.py
@@ -0,0 +1,16 @@
+import os
+from kubemq.queues import *
+
+
+def create_queues_channel(channelName):
+    try:
+        with Client(address="localhost:50000") as client:
+            client.create_queues_channel(channelName)
+            print(f"Queues channel: {channelName} created successfully.")
+    except Exception as e:
+        print(f"Error creating Queues Channel: {channelName},  {e}")
+
+
+
+if __name__ == "__main__":
+    create_queues_channel("new_queues_channel")
