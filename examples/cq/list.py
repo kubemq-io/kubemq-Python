ad9235af5199cda6215b5aa9458740782ff1fbe0
Lior Nabat <lior.nabat@gmail.com>
2024-03-11 13:49:19 +0200

    Add new commands and queries handling script examples
    Introduced new script examples to demonstrate usage of commands and queries in the 'kubemq' module. These scripts emphasize on creating, listing, and deleting channels, and sending query or command requests. Exception handling is also included for each operation to ensure robustness and reliability of the system.

---
+++ examples/cq/list.py
@@ -0,0 +1,24 @@
+import os
+from kubemq.cq import *
+
+
+def list_commands_channels(search_pattern=""):
+    try:
+        with Client(address="localhost:50000", client_id="list_example") as client:
+            list=client.list_commands_channels(search_pattern)
+            print(f"Commands channels list: {list}")
+    except Exception as e:
+        print(f"Error listing Commands Channels: {e}")
+
+
+def list_queries_channels(search_pattern=""):
+    try:
+        with Client(address="localhost:50000", client_id="list_example") as client:
+            list=client.list_queries_channels(search_pattern)
+            print(f"Queries channels list: {list}")
+    except Exception as e:
+        print(f"Error listing Queries Channels: {e}")
+
+if __name__ == "__main__":
+    list_commands_channels()
+    list_queries_channels()
---
+++ examples/cq/queries.py
@@ -0,0 +1,49 @@
+import logging
+import time
+from kubemq.cq import *
+
+def main():
+
+    # try:
+        client = Client(address="localhost:50000")
+        def on_receive_query(request: QueryMessageReceived):
+            try:
+                print(f"Id:{request.id}, Body:{request.body.decode('utf-8')}")
+                response = QueryResponseMessage(
+                    query_received=request,
+                    is_executed=True,
+                    body=b"hello kubemq, I'm replying to you!",
+                )
+                client.send_response_message(response)
+            except Exception as e:
+                print(e)
+
+
+        def on_error_handler(err: str):
+            print(f"Error: {err}")
+
+        client.subscribe_to_queries(
+            subscription=QueriesSubscription(
+                channel="q1",
+                group="",
+                on_receive_query_callback=on_receive_query,
+                on_error_callback=on_error_handler,
+            )
+            , cancel=CancellationToken())
+        time.sleep(1)
+        response: QueryResponseMessage = client.send_query_request(QueryMessage(
+            channel="q1",
+            body=b"hello kubemq, please reply to me!",
+            timeout_in_seconds=10,
+        ))
+        print(
+            f"Request Execution: {response.is_executed}, Body: {response.body} Executed at: {response.timestamp}, Error: {response.error}")
+
+
+    # except Exception as e:
+    #         print(e)
+    #         return
+
+
+if __name__ == "__main__":
+    main()
