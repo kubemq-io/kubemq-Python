dfa7cfedbb5c69459dee76d6114c1967f5c27893
Lior Nabat <lior.nabat@gmail.com>
2024-03-11 12:57:34 +0200

    Refactor pubsub client with improved methods and error handling
    The 'client.py' in kubemq's pubsub module has been refactored to enhance maintenance and readability. It now leverages helper methods for creating, deleting, and listing channels, decreasing the need for explicit in-class logic. Additionally, it utilizes a new gRPC error decoding helper function, improving error troubleshooting in Kubemq cluster based applications.

--- kubemq/pubsub/client.py
+++ kubemq/pubsub/client.py
@@ -4,16 +4,20 @@ import asyncio
 import time
 import uuid
 import grpc
+import socket
 from typing import List
 from kubemq.transport import *
-from kubemq.grpc import (Event,Result,Request,Response,Request)
+from kubemq.grpc import (Event,Result,Request,Response)
 from kubemq.pubsub import *
-from kubemq.common import *
+from kubemq.common.exceptions import *
+from kubemq.common.helpers import *
+from kubemq.common.requests import *
+from kubemq.common.subscribe_type import *
+from kubemq.common.cancellation_token import *
 
-requests_channel="kubemq.cluster.internal.requests"
 class Client:
     def __init__(self, address: str = "",
-                 client_id: str = "",
+                 client_id: str = socket.gethostname(),
                  auth_token: str = "",
                  tls: bool = False,
                  tls_cert_file: str = "",
@@ -101,75 +105,27 @@ class Client:
             ex = GRPCError(e)
             self.logger.error(str(ex))
             raise ex
-
-    def send(self, message: [EventMessage,
-                             EventStoreMessage]) -> [EventSendResult,None]:
+    def send_events_message(self, message: EventMessage) :
+        message.validate()
+        if self.event_sender is None:
+            self.event_sender = EventSender(self.transport, self.shutdown_event, self.logger, self.connection)
+        self.event_sender.send(message.encode(self.connection.client_id))
+    def send_events_store_message(self, message: EventStoreMessage) -> EventSendResult:
         message.validate()
         if self.event_sender is None:
             self.event_sender = EventSender(self.transport, self.shutdown_event, self.logger, self.connection)
         result = self.event_sender.send(message.encode(self.connection.client_id))
         if result is not None:
             return EventSendResult().decode(result)
-        return None
-    def delete_events_channel(self, channel: str):
-        return self._delete_channel(channel, "events")
-    def delete_events_store_channel(self, channel: str):
-        return self._delete_channel(channel, "events_store")
-    def _delete_channel(self, channel: str, channel_type: str):
-        if not channel:
-            raise ValueError("channel cannot be empty")
-        try:
-            self.logger.debug(f"Client deleting {channel_type} channel {channel}")
-            request = Request(
-                RequestID=str(uuid.uuid4()),
-                RequestTypeData=2,
-                Metadata="delete-channel",
-                Channel=requests_channel,
-                ClientID=self.connection.client_id,
-                Body=b'',
-                Tags={"channel": channel, "channel_type": channel_type},
-                Timeout=10 * 1000
-            )
-            response = self.transport.kubemq_client().SendRequest(request)
-            if response:
-                if response.Executed:
-                    self.logger.debug(f"Client deleted {channel_type} channel {channel}")
-                    return True
-                else:
-                    self.logger.error(f"Client failed to delete {channel_type} channel {channel}, error: {response.Error}")
-                    raise DeleteChannelError(response.Error)
-        except grpc.RpcError as e:
-            raise  GRPCError(_decode_grpc_error(e))
 
-    def create_events_channel(self, channel: str):
-        return self._create_channel(channel, "events")
+    def delete_events_channel(self, channel: str):
+        return delete_channel_request(self.transport, self.connection.client_id, channel, "events")
+    def delete_events_store_channel(self, channel: str):
+        return delete_channel_request(self.transport, self.connection.client_id, channel, "events_store")
+    def create_events_channel(self, channel: str)->[bool,None]:
+        return create_channel_request(self.transport, self.connection.client_id, channel, "events")
     def create_events_store_channel(self, channel: str):
-        return self._create_channel(channel, "events_store")
-    def _create_channel(self, channel: str, channel_type: str):
-        if not channel:
-            raise ValueError("channel cannot be empty")
-        try:
-            self.logger.debug(f"Client creating {channel_type} channel {channel}")
-            request = Request(
-                RequestID=str(uuid.uuid4()),
-                RequestTypeData=2,
-                Metadata="create-channel",
-                Channel=requests_channel,
-                ClientID=self.connection.client_id,
-                Body=b'',
-                Tags={"channel": channel, "channel_type": channel_type},
-                Timeout=10 * 1000
-            )
-            response = self.transport.kubemq_client().SendRequest(request)
-            if response:
-                if response.Executed:
-                    self.logger.debug(f"Client created {channel_type} channel {channel}")
-                    return True
-                else:
-                    self.logger.error(f"Client failed to create {channel_type} channel {channel}, error: {response.Error}")
-                    raise CreateChannelError(response.Error)
-        except grpc.RpcError as e:
-            raise GRPCError(_decode_grpc_error(e))
+        return create_channel_request(self.transport, self.connection.client_id, channel, "events_store")
     def list_events_channels(self, channel_search: str = "") -> List[Channel]:
         return self._list_channels("events", channel_search)
     def list_events_store_channels(self, channel_search: str = "") -> List[Channel]:
@@ -195,8 +151,13 @@ class Client:
                     self.logger.error(f"Client failed to list {channel_type} channels, error: {response.Error}")
                     raise ListChannelsError(response.Error)
         except grpc.RpcError as e:
-            raise GRPCError(_decode_grpc_error(e))
-    def subscribe(self,
+            raise GRPCError(decode_grpc_error(e))
+    def subscribe_to_events(self, subscription: EventsSubscription, cancel: CancellationToken = None):
+        self._subscribe(subscription, cancel)
+
+    def subscribe_to_events_store(self, subscription: EventsStoreSubscription, cancel: CancellationToken = None):
+        self._subscribe(subscription, cancel)
+    def _subscribe(self,
                   subscription: [EventsSubscription,EventsStoreSubscription],
                   cancel: [CancellationToken, None]):
         if cancel is None:
@@ -219,11 +180,11 @@ class Client:
                     lambda error: subscription.raise_on_error(error),
                     cancel_token_event)
         threading.Thread(
-            target=self.subscribe_task,
+            target=self._subscribe_task,
             args=args,
             daemon=True).start()
 
-    def subscribe_task(self, stream_callable, decode_callable, error_callable, cancel_token: threading.Event):
+    def _subscribe_task(self, stream_callable, decode_callable, error_callable, cancel_token: threading.Event):
         while not cancel_token.is_set() and not self.shutdown_event.is_set():
             try:
                 response = stream_callable()
@@ -232,24 +193,10 @@ class Client:
                         break
                     decode_callable(message)
             except grpc.RpcError as e:
-                error_callable(_decode_grpc_error(e))
+                error_callable(decode_grpc_error(e))
                 time.sleep(self.connection.reconnect_interval_seconds)
                 continue
             except Exception as e:
-                error_callable(_decode_grpc_error(e))
+                error_callable(decode_grpc_error(e))
                 time.sleep(self.connection.reconnect_interval_seconds)
                 continue
-def _decode_grpc_error(exc) -> str:
-    message = str(exc)
-
-    # Check if the exception has 'code' (status) and 'details' methods
-    if hasattr(exc, 'code') and callable(exc.code) and hasattr(exc, 'details') and callable(exc.details):
-        status = exc.code()
-        details = exc.details()
-        if status == grpc.StatusCode.CANCELLED:
-            return "KubeMQ Connection Closed"
-        # Ensure that status and details are not None
-        if status is not None and details is not None:
-            message = f"KubeMQ Connection Error - Status: {status} Details: {details}"
-
-    return message
