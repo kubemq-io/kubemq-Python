c9ee397f476124f630b8d78fa478198076a78dd4
Lior Nabat <lior.nabat@gmail.com>
2024-03-11 15:29:44 +0200

    Add create, delete, list scripts and refactor queue example
    Three scripts for creating, deleting, and listing queue channels have been added to aid in queue management. Additionally, the 'queues.py' example has been refactored to use the new 'kubemq.queues' module, improving code clarity and efficiency. Lastly, the example files have been reorganized into a new 'queues' directory for better file management.

---
+++ examples/queues/delete.py
@@ -0,0 +1,15 @@
+import os
+from kubemq.queues import *
+
+
+def delete_queues_channel(channelName):
+    try:
+        with Client(address="localhost:50000", client_id="delete_example") as client:
+            client.delete_queues_channel(channelName)
+            print(f"Queues channel: {channelName} deleted successfully.")
+    except Exception as e:
+        print(f"Error deleting Queues Channel: {channelName},  {e}")
+
+
+if __name__ == "__main__":
+    delete_queues_channel("new_queues_channel")
---
+++ examples/queues/queues.py
@@ -0,0 +1,31 @@
+import logging
+from kubemq.queues import *
+
+def main():
+    with Client(address="localhost:50000") as client:
+        send_result= client.send_queues_message(
+                QueueMessage(
+                    channel="q1",
+                    body=b"some-simple_queue-queue-message",
+                    metadata="some-metadata",
+                )
+            )
+        print(f"Queue Message Sent: {send_result}")
+
+        result = client.receive_queues_messages(
+            channel="q1",
+            max_messages=1,
+            wait_timeout_in_seconds=10,
+            auto_ack=False,
+        )
+        if result.is_error:
+            print(f"{result.error}")
+            return
+        for message in result.messages:
+            print(f"Id:{message.id}, Body:{message.body.decode('utf-8')}")
+            message.ack()
+
+
+
+if __name__ == "__main__":
+    main()
