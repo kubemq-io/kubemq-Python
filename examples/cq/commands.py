7037416e637b98822c10f2a65ce36391f68fec48
KubeMQ <lior.nabat@kubemq.io>
2024-03-11 13:49:19 +0200

    Add new commands and queries handling script examples
    Introduced new script examples to demonstrate usage of commands and queries in the 'kubemq' module. These scripts emphasize on creating, listing, and deleting channels, and sending query or command requests. Exception handling is also included for each operation to ensure robustness and reliability of the system.

---
+++ examples/cq/commands.py
@@ -0,0 +1,43 @@
+import logging
+import time
+from kubemq.cq import *
+
+def main():
+
+    try:
+        client = Client(address="localhost:50000")
+        def on_receive_command(request: CommandMessageReceived):
+            try:
+                print(f"Id:{request.id}, Body:{request.body.decode('utf-8')}")
+                response = CommandResponseMessage(
+                    command_received=request,
+                    is_executed=True,
+                )
+                client.send_response_message(response)
+            except Exception as e:
+                print(e)
+        def on_error_handler(err: str):
+            print(f"{err}")
+
+        client.subscribe_to_commands(
+            subscription=CommandsSubscription(
+                channel="c1",
+                group="",
+                on_receive_command_callback=on_receive_command,
+                on_error_callback=on_error_handler,
+            )
+            , cancel=CancellationToken())
+        time.sleep(1)
+        response = client.send_command_request(CommandMessage(
+            channel="c1",
+            body=b"hello kubemq, please reply to me!",
+            timeout_in_seconds=10,
+        ))
+        print(f"Request Execution: {response.is_executed}, Executed at: {response.timestamp}, Error: {response.error}")
+    except Exception as e:
+            print(e)
+            return
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/cq/delete.py
@@ -0,0 +1,24 @@
+import os
+from kubemq.cq import *
+
+
+def delete_commands_channel(channelName):
+    try:
+        with Client(address="localhost:50000", client_id="delete_example") as client:
+            client.delete_commands_channel(channelName)
+            print(f"Commands channel: {channelName} deleted successfully.")
+    except Exception as e:
+        print(f"Error deleting Commands Channel: {channelName},  {e}")
+
+
+def delete_queries_channel(channelName):
+    try:
+        with Client(address="localhost:50000", client_id="delete_example") as client:
+            client.delete_queries_channel(channelName)
+            print(f"Queries channel: {channelName} deleted successfully.")
+    except Exception as e:
+        print(f"Error creating Queries Channel: {channelName},  {e}")
+
+if __name__ == "__main__":
+    delete_commands_channel("new_commands_channel")
+    delete_queries_channel("new_queries_channel")
