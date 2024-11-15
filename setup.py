7af82543cf4a1d58d9babe20c0ed3861ccb17e9b
KubeMQ <lior.nabat@kubemq.io>
2024-11-15 14:52:44 +0200

    3.4.1 - fix query response body

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="kubemq",
-    version="3.4.0",
+    version="3.4.1",
     description="KubeMQ SDK for Python",
     long_description=README,
     long_description_content_type="text/markdown",
